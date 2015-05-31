8c0e7e7c2e1616934b0d494527d730447fdf0112
Gnamra <Gnamra@gmail.com>
2015-06-01 01:18:09 +0200

    Continued on A* and prepared dumbot to follow the path

---
+++ General/PriorityQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG4500_2015_Innlevering2.General
+{
+    public class PriorityQueue<T>
+    {
+        private List<Tuple<T, int>> elements = new List<Tuple<T, int>>();
+
+        public int Count
+        {
+            get { return elements.Count; }
+        }
+
+        public void Enqueue(T item, int priority)
+        {
+            elements.Add(Tuple.Create(item, priority));
+        }
+
+        public T Dequeue()
+        {
+            int bestIndex = 0;
+
+            for(int i = 0; i < elements.Count; i++)
+            {
+                if(elements[i].Item2 < elements[bestIndex].Item2)
+                {
+                    bestIndex = i;
+                }
+            }
+
+            T item = elements[bestIndex].Item1;
+            elements.RemoveAt(bestIndex);
+            return item;
+        }
+    }
+}
